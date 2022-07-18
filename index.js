05ab40a521ffe234b2fdccdbd1df5d7b61474015
kundansankhe <kundansankhe@gmail.com>
2022-07-18 16:32:43 +0530

    working on book library

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("this is book library project");
+
+// create constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// display constructor
+function Display() {
+
+}
+
